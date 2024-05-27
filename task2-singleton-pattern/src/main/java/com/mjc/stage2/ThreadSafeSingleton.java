651d018b9c011d2877a6ba711253ab6ef66c6f63
fenixtheidragon <fenixtheidragon@gmail.com>
2024-05-27 12:47:04 +0300

    initial;

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,4 +2,12 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
     // Write your code here!
+  private final static ThreadSafeSingleton threadSafeSingleton= new ThreadSafeSingleton();
+  private ThreadSafeSingleton() {
+
+  }
+
+  public ThreadSafeSingleton getInstance() {
+    return this;
+  }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,27 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2ConnectionFactory implements ConnectionFactory {
     // Write your code here!
+  public H2ConnectionFactory() {
+
+  }
+
+  @Override
+  public Connection createConnection() {
+    Connection connection;
+    try {
+      connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null;
+    }
+    return connection;
+  }
 }
 
